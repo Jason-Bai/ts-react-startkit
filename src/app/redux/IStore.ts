199541ead38c82312772c2996b75ac5fdb4a8d0f
baiyu <baiyu@admaster.com.cn>
2018-08-18 17:01:51 +0800

    vortigern

---
+++ src/app/redux/IStore.ts
@@ -0,0 +1,9 @@
+import { ICounter } from 'models/counter';
+import { IStars } from 'models/stars';
+
+export interface IStore {
+  counter: ICounter;
+  stars: IStars;
+  routing: any;
+  reduxAsyncConnect: any;
+};
